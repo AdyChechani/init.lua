b7b233026fe0447cb84e76f271cf402635fbf063
achechani <achechani@cloudera.com>
2025-09-02 19:52:05 +0530

    Some changes

--- lua/adych/init.lua
+++ lua/adych/init.lua
@@ -46,6 +46,16 @@ vim.api.nvim_create_autocmd("FileType", {
         end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+        pattern = "xml",
+        callback = function ()
+                vim.opt_local.tabstop = 4
+                vim.opt_local.softtabstop = 4
+                vim.opt_local.shiftwidth = 4
+                vim.opt_local.expandtab = true
+        end,
+})
+
 -- -- Format Python files with black on save
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --         pattern = "*.py",
